392f4190572fd54286969c8a51137f638f7a4b7b
Darrel <darrel124568@gmail.com>
2026-07-02 12:41:38 +0300

    Implement LinkedList class with basic operations and update main.py for testing

--- main.py
+++ main.py
@@ -7,20 +7,27 @@ if __name__ == "__main__":
     """
 
     # TODO: 1) Create a LinkedList instance
+    ll = LinkedList()
     
 
     # TODO: 2) Insert some sample data using insert_at_front or insert_at_end
-    
+    ll.insert_at_front(5)
+    ll.insert_at_front(10)
+    ll.insert_at_end(15)
+
     # TODO: 3) Display the list to verify insertion
-    
+    ll.display()
 
     # TODO: 4) Call recursive_sum and print the result
-    
+    print("Sum:", ll.recursive_sum())
 
     # TODO: 5) Call recursive_search with a target and print result
-    
+    print("Search 10:", ll.recursive_search(10))
+    print("Search 20:", ll.recursive_search(20))
 
     # TODO: 6) Call recursive_reverse, then display the reversed list
+    ll.recursive_reverse()
+    ll.display()
     
 
 
